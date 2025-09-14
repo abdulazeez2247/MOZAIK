2118e2f8bfd1984af6b82e3d82827e3c4ebf7915
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-09-14 04:38:39 +0100

    changeapiurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:9000/api";
+// const API_URL = "http://localhost:9000/api";
+const API_URL = "https://moe-backend-3.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
